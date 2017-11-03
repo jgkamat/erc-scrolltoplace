937949ac2bb72ba2a788ee2d30135bd67046dba8
Jay Kamat <jaygkamat@gmail.com>
2017-11-03 01:13:59 -0400

    Fix a few header errors

--- erc-scrolltoplace.el
+++ erc-scrolltoplace.el
@@ -3,9 +3,9 @@
 ;; Copyright (C) 2017 Jay Kamat
 ;; Author: Jay Kamat <jaygkamat@gmail.com>
 ;; Version: 0.0.1
-;; Keywords: erc, module, scrolltobottom
+;; Keywords: erc, module, comm, scrolltobottom, keep-place
 ;; URL: http://github.com/jgkamat/erc-scrolltoplace
-;; Package-Requires: ((emacs "24.0") (switch-buffer-functions))
+;; Package-Requires: ((emacs "24.0") (switch-buffer-functions "0.0.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
